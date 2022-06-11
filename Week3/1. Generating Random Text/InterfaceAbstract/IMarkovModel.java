e1b55389d355f174245f8bcf9e38878212c9b3ff
a88126451 <49521685+a88126451@users.noreply.github.com>
2022-06-11 19:44:13 +0800

    Week3

---
+++ Week3/1. Generating Random Text/InterfaceAbstract/IMarkovModel.java
@@ -0,0 +1,9 @@
+
+public interface IMarkovModel {
+    public void setTraining(String text);
+    
+    public String getRandomText(int numChars);
+    
+    public void setRandom(int seed);
+    
+}
